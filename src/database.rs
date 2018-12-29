0a59f098b3b9e737d85bb043e3b11fb29b0f0045
Tangent 128 <Tangent128@gmail.com>
2018-12-28 23:17:03 -0500

    Extract data access to a service module

---
+++ src/database.rs
@@ -0,0 +1,44 @@
+/// Service responsible for managing database queries.
+
+embed_migrations!();
+
+use std::sync::{Mutex, MutexGuard};
+
+use diesel::prelude::*;
+
+#[derive(Queryable)]
+pub struct Player {
+    pub id: i32,
+    pub url_key: String,
+    pub name: String
+}
+
+pub struct Database {
+    /// Due to low anticipated load, just keep a single database connection
+    /// instead of setting up an r2d2 connection pool for now
+    conn: Mutex<SqliteConnection>
+}
+
+impl Database {
+    pub fn connect(filename: &str) -> Database {
+        let conn = SqliteConnection::establish(filename).expect("Opening Sqlite");
+
+        // Initialize database
+        embedded_migrations::run_with_output(&conn, &mut std::io::stdout()).expect("Running migrations");
+
+        Database {
+            conn: Mutex::new(conn)
+        }
+    }
+
+    fn conn(&self) -> MutexGuard<SqliteConnection> {
+        self.conn.lock().unwrap()
+    }
+
+    pub fn player_from_key(&self, key: &str) -> QueryResult<Option<Player>> {
+        use crate::schema::player::dsl::*;
+        player.filter(urlKey.eq(key))
+            .get_result(&*self.conn()).optional()
+    }
+}
+
--- src/main.rs
+++ src/main.rs
@@ -2,40 +2,24 @@
 #[macro_use] extern crate diesel_migrations;
 #[macro_use] extern crate tower_web;
 
-use std::sync::Mutex;
-
 use clap::{App, Arg};
 use diesel::prelude::*;
 use tower_web::ServiceBuilder;
 
+mod database;
 mod schema;
 
-use self::schema::player;
-
-embed_migrations!();
+use crate::database::{Database};
 
 struct CloudHatWeb {
-    /// Due to low anticipated load, just keep a single database connection
-    /// instead of setting up an r2d2 connection pool for now
-    conn: Mutex<SqliteConnection>
-}
-
-#[derive(Queryable)]
-struct Player {
-    id: i32,
-    #[column_name = "urlKey"]
-    url_key: String,
-    name: String
+    db: Database
 }
 
 impl_web! {
     impl CloudHatWeb {
         #[get("/player/:key")]
         fn show_player(&self, key: String) -> QueryResult<String> {
-            let player: QueryResult<Option<Player>> = player::table
-                .filter(player::urlKey.eq(key))
-                .get_result(&*self.conn.lock().unwrap()).optional();
-            player.map(|option| match option {
+            self.db.player_from_key(&key).map(|option| match option {
                 Some(player) => format!("Player's name is {}", player.name),
                 None => "Player does not exist, this should really be a 404 silly".into()
             })
@@ -51,16 +35,12 @@ fn main() {
 
     let args = usage.get_matches();
 
-    let conn = SqliteConnection::establish(args.value_of("database").unwrap()).expect("Opening Sqlite");
     let listen_to = args.value_of("address").unwrap().parse().expect("Listen address");
-
-    // Initialize database
-    embedded_migrations::run_with_output(&conn, &mut std::io::stdout()).expect("Running migrations");
 
     // Launch server
     ServiceBuilder::new()
         .resource(CloudHatWeb {
-            conn: Mutex::new(conn)
+            db: Database::connect(args.value_of("database").unwrap())
         })
         .run(&listen_to)
         .unwrap();
