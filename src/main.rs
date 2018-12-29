78cfbe982ebdf6641f0dfbe8bee046617efd9f0a
Tangent 128 <Tangent128@gmail.com>
2018-12-28 22:04:52 -0500

    Test web controller connecting to the database

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,44 @@
+#[macro_use] extern crate diesel;
 #[macro_use] extern crate diesel_migrations;
 #[macro_use] extern crate tower_web;
 
+use std::sync::Mutex;
+
 use clap::{App, Arg};
 use diesel::prelude::*;
-
 use tower_web::ServiceBuilder;
+
+mod schema;
+
+use self::schema::player;
 
 embed_migrations!();
 
 struct CloudHatWeb {
+    /// Due to low anticipated load, just keep a single database connection
+    /// instead of setting up an r2d2 connection pool for now
+    conn: Mutex<SqliteConnection>
+}
+
+#[derive(Queryable)]
+struct Player {
+    id: i32,
+    #[column_name = "urlKey"]
+    url_key: String,
+    name: String
 }
 
 impl_web! {
     impl CloudHatWeb {
         #[get("/player/:key")]
-        fn show_player(&self, _key: String) -> Result<String, ()> {
-            Ok("Hello, World".into())
+        fn show_player(&self, key: String) -> QueryResult<String> {
+            let player: QueryResult<Option<Player>> = player::table
+                .filter(player::urlKey.eq(key))
+                .get_result(&*self.conn.lock().unwrap()).optional();
+            player.map(|option| match option {
+                Some(player) => format!("Player's name is {}", player.name),
+                None => "Player does not exist, this should really be a 404 silly".into()
+            })
         }
     }
 }
@@ -37,6 +60,7 @@ fn main() {
     // Launch server
     ServiceBuilder::new()
         .resource(CloudHatWeb {
+            conn: Mutex::new(conn)
         })
         .run(&listen_to)
         .unwrap();
